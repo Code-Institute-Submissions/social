bde03971f1958bdf7669cd89947c025927a931f4
Jacob Shortall <70966631+jacobshortall@users.noreply.github.com>
2021-12-16 18:39:01 +0000

    Delete env.py from GitHub

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ['DATABASE_URL'] = 'postgres://aadanzzdnmyuwk:b28aed73a6d731a47166378ec00f02fa4aadcc79feb56063391a9b480cec7786@ec2-176-34-105-15.eu-west-1.compute.amazonaws.com:5432/de55nfun60ttq'
-os.environ['SECRET_KEY'] = 'ToTallysecret'
-os.environ['CLOUDINARY_URL'] = 'cloudinary://513886536914333:vD9niINv8xwXLQFdwNw-PjRIlVA@dj3brkru2'
