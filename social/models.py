727c5b861125ea39971cebe865d717c2694f291b
Jacob <Jacobshortall@gmail.com>
2021-12-17 18:16:52 +0000

    Create application models

--- social/models.py
+++ social/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db.models.fields import DateTimeField
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts')
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='posts')
+    post_date = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='post_like', blank=True)
+    image = CloudinaryField('image', blank=True)
+
+    class Meta:
+        ordering = ['-post_date']
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    comment_date = DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['comment_date']
+
+    def __str__(self):
+        return self.content
