8a3a7bc30a0e4a2db78aeec67d6673935c29b4f6
Jacob <Jacobshortall@gmail.com>
2021-12-19 13:03:12 +0000

    Set x_frame_origin to test admin in deployment

--- social_app/settings.py
+++ social_app/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['127.0.0.1', 'ci-social.herokuapp.com']
 
 
