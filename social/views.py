579625b4cfa8aeba41ea37ca88b6ba5adf60078c
Jacob <Jacobshortall@gmail.com>
2021-12-23 19:54:38 +0000

    Add first view with basic navbar to be used as base template

--- social/views.py
+++ social/views.py
@@ -1,3 +1,12 @@
+from django import http
+from django.db.models import query
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+
+def home(request):
+
+    return render(request, 'index.html', {
+        'p': range(100),
+    })
