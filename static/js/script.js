169d73a8d9928f7edad035668df6d144c7090776
Jacob <Jacobshortall@gmail.com>
2022-01-02 18:08:57 +0000

    Format documents

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
     /** Toggle navigation */
-    $("#nav-toggle").click( function() {
-        arrowToggle()
-        $("#nav-collapse").toggle(500)
-    })
+    $("#nav-toggle").click(function () {
+        arrowToggle();
+        $("#nav-collapse").toggle(500);
+    });
 });
 
 /** Check current status of arrow nav toggler and change it to the opposite.*/
 function arrowToggle() {
     if ($("#toggle-icon").hasClass("fa-arrow-circle-right")) {
-        $("#toggle-icon").removeClass("fa-arrow-circle-right").addClass("fa-arrow-circle-left")
-    }
-    else {
-        $("#toggle-icon").removeClass("fa-arrow-circle-left").addClass("fa-arrow-circle-right")
+        $("#toggle-icon")
+            .removeClass("fa-arrow-circle-right")
+            .addClass("fa-arrow-circle-left");
+    } else {
+        $("#toggle-icon")
+            .removeClass("fa-arrow-circle-left")
+            .addClass("fa-arrow-circle-right");
     }
 }
