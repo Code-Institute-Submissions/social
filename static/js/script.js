622ed34753fe12473bfd959a5410a432e9539575
Jacob <Jacobshortall@gmail.com>
2022-01-02 17:13:03 +0000

    Add responsive, animated toggling sidebar using jQuery

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    /** Toggle navigation */
+    $("#nav-toggle").click( function() {
+        arrowToggle()
+        $("#nav-collapse").toggle(500)
+    })
+});
+
+/** Check current status of arrow nav toggler and change it to the opposite.*/
+function arrowToggle() {
+    if ($("#toggle-icon").hasClass("fa-arrow-circle-right")) {
+        $("#toggle-icon").removeClass("fa-arrow-circle-right").addClass("fa-arrow-circle-left")
+    }
+    else {
+        $("#toggle-icon").removeClass("fa-arrow-circle-left").addClass("fa-arrow-circle-right")
+    }
+}
