f3b539f6123a4bb70242a8d9638b0b7343e6ba1d
Jacob <Jacobshortall@gmail.com>
2022-01-22 16:51:49 +0000

    Add backend logic to process 'add comment' POST request

--- social/views.py
+++ social/views.py
@@ -30,3 +30,24 @@ class PostView(View):
             'comments': comments,
             'comment_form': CommentForm()
         })
+
+    def post(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        comments = post.comments.order_by('-comment_date')
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.author = request.user
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(request, 'post_view.html', {
+            'post': post,
+            'comments': comments,
+            'comment_form': CommentForm()
+        })
+
